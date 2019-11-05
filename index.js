e5b16cd6fe01054a6a02c82c1255ba3557135802
Ian Diesendruck <ian.diesendruck@wework.com>
2019-11-05 13:56:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = d + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
+
+var d = "Hello,"
